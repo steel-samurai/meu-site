b89a9e15821b6d0394f4a6316f49f11f80654a3b
iza <125701967+steel-samurai@users.noreply.github.com>
2024-01-24 16:30:10 -0300

    update de java.js

--- java.js
+++ java.js
@@ -1,30 +1,43 @@
+window.onscroll = function () { myFunction() };
+
+var header = document.getElementById("aheader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.scrollY > sticky) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+}
+
 
 document.addEventListener('DOMContentLoaded', function () {
-    const buttons = document.querySelectorAll('button');
-    const textContents = [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae justo eget magna fermentum. Imperdiet nulla malesuada pellentesque elit eget gravida cum.",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae justo eget magna fermentum. Imperdiet nulla malesuada pellentesque elit eget gravida cum.",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae justo eget magna fermentum. Imperdiet nulla malesuada pellentesque elit eget gravida cum.",
-    ];
+  const buttons = document.querySelectorAll('button');
+  const textContents = [
+    "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae justo eget magna fermentum. Imperdiet nulla malesuada pellentesque elit eget gravida cum.",
+    "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae justo eget magna fermentum. Imperdiet nulla malesuada pellentesque elit eget gravida cum.",
+    "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae justo eget magna fermentum. Imperdiet nulla malesuada pellentesque elit eget gravida cum.",
+  ];
 
-    buttons.forEach((button, index) => {
-      button.addEventListener('click', function () {
-        const textContainer = document.getElementById(button.id + '-text');
-        const newTextContent = textContents[index];
+  buttons.forEach((button, index) => {
+    button.addEventListener('click', function () {
+      const textContainer = document.getElementById(button.id + '-text');
+      const newTextContent = textContents[index];
 
-        if (textContainer.style.display === 'block') {
-          textContainer.style.display = 'none';
-        } else {
-          // Hide all other text containers
-          document.querySelectorAll('.text-container').forEach(container => {
-            if (container !== textContainer) {
-              container.style.display = 'none';
-            }
-          });
+      if (textContainer.style.display === 'block') {
+        textContainer.style.display = 'none';
+      } else {
+        // Hide all other text containers
+        document.querySelectorAll('.text-container').forEach(container => {
+          if (container !== textContainer) {
+            container.style.display = 'none';
+          }
+        });
 
-          textContainer.style.display = 'block';
-          textContainer.innerText = newTextContent;
-        }
-      });
+        textContainer.style.display = 'block';
+        textContainer.innerText = newTextContent;
+      }
     });
   });
+});
