4cdc0ee6a9a4591f104ca0e97c5009a0de9495b8
iza <125701967+steel-samurai@users.noreply.github.com>
2024-01-26 22:47:36 -0300

    Update java.js

--- java.js
+++ java.js
@@ -10,7 +10,7 @@ function myFunction() {
     header.classList.remove("fixed");
   }
 }
-
+ 
 
 document.addEventListener('DOMContentLoaded', function () {
   const buttons = document.querySelectorAll('button');
@@ -61,3 +61,89 @@ document.addEventListener('scroll', function () {
   var num = Math.min(Math.max(scrollDistance, minTop), maxTop);
   movingElement.style.top = num + 'px';
 });
+
+
+document.getElementById('btn1').addEventListener('click', function() {
+  changeText(1);
+});
+
+document.getElementById('btn2').addEventListener('click', function() {
+  changeText(2);
+});
+
+function changeText(btnNumber) {
+  var textoDiv = document.getElementById('texto');
+  var btn1 = document.getElementById('btn1');
+  var btn2 = document.getElementById('btn2');
+
+  if (btnNumber === 1) {
+    textoDiv.innerHTML = 'Novo texto do Botão 1';
+    btn1.classList.add('selected');
+    btn2.classList.remove('selected');
+  } else if (btnNumber === 2) {
+    textoDiv.innerHTML = 'Novo texto do Botão 2';
+    btn1.classList.remove('selected');
+    btn2.classList.add('selected');
+  }
+}
+
+
+
+
+let currentIndex = 0;
+const images = [
+    "imagem1.png",
+    "imagem2.png",
+    "imagem3.png",
+];
+
+const imageElement = document.getElementById('image');
+const prevButton = document.getElementById('prevBtn');
+const nextButton = document.getElementById('nextBtn');
+const imageCaption = document.createElement('div');
+
+imageCaption.id = 'imageCaption';
+document.getElementById('carousel').appendChild(imageCaption);
+
+// Adiciona eventos de clique aos botões
+prevButton.onclick = function() {
+    changeImage(-1);
+};
+
+nextButton.onclick = function() {
+    changeImage(1);
+};
+
+imageElement.onmouseover = function() {
+    showCaption();
+};
+
+imageElement.onmouseout = function() {
+    hideCaption();
+};
+
+function showCaption() {
+    imageCaption.style.opacity = 0.9;
+    imageCaption.innerHTML = `Legenda da Imagem ${currentIndex + 1}`;
+}
+
+function hideCaption() {
+    imageCaption.style.opacity = 0;
+}
+
+function changeImage(indexIncrement) {
+    currentIndex += indexIncrement;
+
+    // Verifica se atingiu o final ou o início do array
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    } else if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+
+    // Atualiza a imagem exibida
+    imageElement.src = images[currentIndex];
+
+    // Atualiza a legenda
+    showCaption();
+}
